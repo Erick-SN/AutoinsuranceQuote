b95f2457611ce890772656c77b6dae622fcb76c0
Erick-SN <ericksn0707@gmail.com>
2020-06-26 11:08:38 -0500

    Using style commponents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const headerContainer = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffff;
+`;
+
+const headerTecxt = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: 'Slabo 27px', serif;
+  text-align: center;
+`;
+
 const Header = ({ title }) => {
   return (
     <>
-      <header>
-        <h1>{title}</h1>
-      </header>
+      <headerContainer>
+        <headerTecxt>{title}</headerTecxt>
+      </headerContainer>
     </>
   );
 };
